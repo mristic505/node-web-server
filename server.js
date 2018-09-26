4314e18cd087a31cdfc0bd6477cf85c1eab4ee11
Mateja Ristic <mristic@brandconnections.com>
2018-09-26 01:46:59 -0400

    first commit

---
+++ server.js
@@ -0,0 +1,57 @@
+const express = require('express');
+const hbs = require('hbs');
+const fs = require('fs');
+
+const app = express();
+
+// Handlebars partials
+hbs.registerPartials(__dirname + '/views/partials');
+app.set('view engine', 'hbs');
+
+// Middlewear
+app.use(express.static(__dirname + '/public'));
+
+app.use((req, res, next) => {
+	var now = new Date().toString();
+	var log = `${now}: ${req.method} ${req.url}`	
+	console.log(log);
+	fs.appendFile('server.log', log + '\n', (err) => {
+		if(err) {
+			console.log('unable to write to server.log');
+		}
+	});
+	next();
+});
+
+// Handlebars Helpers
+hbs.registerHelper('getCurrentYear', () => {
+	return new Date().getFullYear();
+});
+hbs.registerHelper('screamIt', (text) => {
+	return text.toUpperCase();
+});
+
+// Routing
+app.get('/', (req, res) => {
+	res.render('home.hbs', {
+		pageTitle: 'Home Page',
+		welcomeMessage: 'Welcome to my castle!'
+	});
+});
+
+app.get('/about', (req, res) => {	
+	res.render('about.hbs', {
+		pageTitle: 'About Page'	
+	});
+});
+
+app.get('/bad', (req, res) => {
+	res.send({
+		errorMessage: 'Shit went wrong'
+	});
+});
+
+// Define port
+app.listen(3000, () => {
+	console.log('running on port 3000');
+}); 
